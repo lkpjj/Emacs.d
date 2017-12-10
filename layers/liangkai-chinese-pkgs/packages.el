5c4025427615fcb2e77e1ded0424edc186822d8d
liangkai <kevin.scnu@gmail.com>
2017-12-10 12:32:43 +0800

    add pangu-spacing package

--- layers/liangkai-chinese-pkgs/packages.el
+++ layers/liangkai-chinese-pkgs/packages.el
@@ -14,6 +14,7 @@
 (defconst liangkai-chinese-pkgs-packages
   '(
     youdao-dictionary
+    pangu-spacing
     pyim
     ))
 
@@ -45,25 +46,23 @@
       (setq default-input-method "pyim")
       (setq pyim-default-scheme 'quanpin)
       (setq pyim-page-tooltip t)
-      ;; ;; 设置 pyim 探针设置，这是 pyim 高级功能设置，可以实现 *无痛* 中英文切换 :-)
-      ;; ;; 我自己使用的中英文动态切换规则是：
-      ;; ;; 1. 光标只有在注释里面时，才可以输入中文。
-      ;; ;; 2. 光标前是汉字字符时，才能输入中文。
-      ;; ;; 3. 使用 M-j 快捷键，强制将光标前的拼音字符串转换为中文。
-      ;; (setq-default pyim-english-input-switch-functions
-      ;;               '(pyim-probe-dynamic-english
-      ;;                 pyim-probe-isearch-mode
-      ;;                 pyim-probe-program-mode
-      ;;                 pyim-probe-org-structure-template))
-      ;; (setq-default pyim-punctuation-half-width-functions
-      ;;               '(pyim-probe-punctuation-line-beginning
-      ;;                 pyim-probe-punctuation-after-punctuation))
-
-      ;; 选词框显示5个候选词
+      ;; 选词框显示6个候选词
       (setq pyim-page-length 6)
       ;; 让 Emacs 启动时自动加载 pyim 词库
       (add-hook 'emacs-startup-hook
                 #'(lambda () (pyim-restart-1 t)))
+      ;; 如果当前的 mode 衍生自 prog-mode，那么仅仅在字符串和 comment 中开启中文输入模式
+      (setq-default pyim-english-input-switch-functions
+                    '(pyim-probe-program-mode))
       (evilified-state-evilify pyim-dm-mode pyim-dm-mode-map))))
 
+(defun liangkai-chinese-pkgs/init-pangu-spacing ()
+  (use-package pangu-spacing
+    :defer t
+    :init (progn (global-pangu-spacing-mode 1)
+                 (spacemacs|hide-lighter pangu-spacing-mode)
+                 ;; Always insert `real' space in org-mode.
+                 (add-hook 'org-mode-hook
+                           '(lambda ()
+                              (set (make-local-variable 'pangu-spacing-real-insert-separtor) t))))))
 ;;; packages.el ends here
